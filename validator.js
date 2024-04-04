0cb5dd7f70b3d37aed47117c30465b02450bf9ff
Your Name <you@example.com>
2024-04-05 01:25:10 +0700

    Tạo thêm các rule cho mỗi ô input

--- validator.js
+++ validator.js
@@ -1,9 +1,22 @@
 // Đối tượng `Validator`
 function Validator(options) {
+    var selectorRule = {};
+
     // Hàm thực hiện validate
     function validate(inputElement, rule) {
-        var errorMessage = rule.test(inputElement.value);
-        var errorElement = inputElement.parentElement.querySelector(options.errorSelector);
+        var errorMessage;
+        var errorElement = inputElement.parentElement.querySelector(
+            options.errorSelector
+        );
+
+        var rules = selectorRule[rule.selector];
+        for (i = 0; i < rules.length; i++) {
+            errorMessage = rules[i](inputElement.value); 
+            if (errorMessage) {
+                break;
+            }
+        }
+
         if (errorMessage) {
             errorElement.innerText = errorMessage;
             inputElement.parentElement.classList.add("invalid");
@@ -16,7 +29,16 @@ function Validator(options) {
     var formElement = document.querySelector(options.form);
     if (formElement) {
         options.rules.forEach(function (rule) {
+            console.log(selectorRule[rule.selector]);
+            // Lưu lại các rule cho mỗi input
+            if (Array.isArray(selectorRule[rule.selector])) {
+                selectorRule[rule.selector].push(rule.test);
+            } else {
+                selectorRule[rule.selector] = [rule.test];
+            }
+
             var inputElement = formElement.querySelector(rule.selector);
+            // console.log(inputElement);
 
             if (inputElement) {
                 // Xử lý trường hợp khi blur khỏi Input
@@ -26,41 +48,61 @@ function Validator(options) {
 
                 // Xử lý mỗi khi người dùng nhập vào input
                 inputElement.oninput = function () {
-                    var errorElement = inputElement.parentElement.querySelector(options.errorSelector);                   
+                    var errorElement = inputElement.parentElement.querySelector(
+                        options.errorSelector
+                    );
                     errorElement.innerText = "";
                     inputElement.parentElement.classList.remove("invalid");
                 };
             }
         });
+        console.log(selectorRule);
     }
 }
 
 // Định nghĩa rules
 // Kiểm tra xem người dùng đã nhập chưa
-Validator.isRequired = function (selector) {
+Validator.isRequired = function (selector, message) {
     return {
         selector: selector,
         test: function (value) {
-            return value.trim() ? undefined : "Vui lòng nhập thông tin";
+            return value.trim()
+                ? undefined
+                : message || "Vui lòng nhập thông tin";
         },
     };
 };
 // Kiểm tra xem đây có phải là email không
-Validator.isEmail = function (selector) {
+Validator.isEmail = function (selector, message) {
     return {
         selector: selector,
         test: function (value) {
             var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-            return regex.test(value) ? undefined : "Trường này phải là email";
+            return regex.test(value)
+                ? undefined
+                : message || "Trường này phải là email";
         },
     };
 };
 
-Validator.minLength = function (selector, min) {
+Validator.minLength = function (selector, min, message) {
     return {
         selector: selector,
         test: function (value) {
-            return value.length >= min ? undefined : `Vui lòng nhập tối thiểu ${min} ký tự`;
+            return value.length >= min
+                ? undefined
+                : message || `Vui lòng nhập tối thiểu ${min} ký tự`;
+        },
+    };
+};
+
+Validator.isConfirmed = function (selector, getConfirmValue, message) {
+    return {
+        selector: selector,
+        test: function (value) {
+            return value === getConfirmValue()
+                ? undefined
+                : message || "Giá trị nhập vào không chính xác";
         },
     };
 };
